aae306bda8c89364b0bca59f758f08643f554167
David Wightman <david@davidwightman.org>
2018-07-28 23:09:06 -0400

    finished logic for index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 removeTransition = (e) => {
-    if (e.properyName !== 'transform') return;
+    if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
 }
 
@@ -8,7 +8,11 @@ playSound = (e) => {
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
 
-    key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
+    key.classList.add('playing');
 }
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
